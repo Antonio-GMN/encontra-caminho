1b7ce96beed6e9b09129ca6b83b6a7e21e93c3e6
Antonio Garcia <antonio.garciamn@gmail.com>
2026-06-15 16:28:06 -0300

    mapa

--- src/dados.ts
+++ src/dados.ts
@@ -1,50 +1,51 @@
 type ArestaType = {
-    distancia: number;
-    perigo: number;
+  distancia: number;
+  perigo: number;
 };
 
 type BibliotecaType = {
-    [key: string]: {
-        [vizinho: string]: ArestaType;
-    };
+  [key: string]: {
+    [vizinho: string]: ArestaType;
+  };
 };
 
 const biblioteca: BibliotecaType = {
-    A: {
-        B: { distancia: 4, perigo: 3 },
-        C: { distancia: 2, perigo: 1 }
-    },
-    B: {
-        A: { distancia: 4, perigo: 3 },
-        C: { distancia: 1, perigo: 2 },
-        D: { distancia: 5, perigo: 6 }
-    },
-    C: {
-        A: { distancia: 2, perigo: 1 },
-        B: { distancia: 1, perigo: 2 },
-        D: { distancia: 8, perigo: 8 },
-        E: { distancia: 10, perigo: 10 }
-    },
-    D: {
-        B: { distancia: 5, perigo: 6 },
-        C: { distancia: 8, perigo: 8 },
-        E: { distancia: 2, perigo: 2 },
-        F: { distancia: 6, perigo: 9 }
-    },
-    E: {
-        C: { distancia: 10, perigo: 10 },
-        D: { distancia: 2, perigo: 2 },
-        F: { distancia: 3, perigo: 4 }
-    },
-    F: {
-        D: { distancia: 6, perigo: 9 },
-        E: { distancia: 3, perigo: 4 }
-    }
+  A: {
+    B: { distancia: 2.4, perigo: 3 },
+    E: { distancia: 4.2, perigo: 4 },
+  },
+  B: {
+    A: { distancia: 2.4, perigo: 3 },
+    C: { distancia: 1.8, perigo: 2 },
+    D: { distancia: 2.2, perigo: 4 },
+  },
+  C: {
+    B: { distancia: 1.8, perigo: 2 },
+    D: { distancia: 1.6, perigo: 2 },
+    G: { distancia: 9.2, perigo: 7 },
+  },
+  D: {
+    B: { distancia: 2.2, perigo: 4 },
+    C: { distancia: 1.6, perigo: 2 },
+    E: { distancia: 2.8, perigo: 3 },
+    F: { distancia: 5.0, perigo: 6 },
+  },
+  E: {
+    A: { distancia: 4.2, perigo: 4 },
+    D: { distancia: 2.8, perigo: 3 },
+    F: { distancia: 5.2, perigo: 5 },
+  },
+  F: {
+    D: { distancia: 5.0, perigo: 6 },
+    E: { distancia: 5.2, perigo: 5 },
+    G: { distancia: 7.6, perigo: 6 },
+  },
+  G: {
+    C: { distancia: 9.2, perigo: 7 },
+    F: { distancia: 7.6, perigo: 6 },
+  },
 };
 
 export { biblioteca };
 
-export type {
-    BibliotecaType,
-    ArestaType
-};
+export type { BibliotecaType, ArestaType };
